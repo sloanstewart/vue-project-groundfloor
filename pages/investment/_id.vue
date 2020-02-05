ec8c00c5c903ba76fe5fe85f06846b6c19797e4a
sloanstewart <sloanstewartdev@gmail.com>
2020-02-05 18:30:17 -0500

    add invest form and show funds

--- pages/investment/_id.vue
+++ pages/investment/_id.vue
@@ -4,24 +4,52 @@
     <p>Address: {{ investment.address }}</p>
     <p>Purpose: {{ investment.purpose }}</p>
 
+    <section>
+      <b-form @submit="onSubmit">
+        <b-form-group label="Investment Amount" label-for="investment_amount">
+          <b-input-group>
+            <b-input-group-prepend>
+              <span class="input-group-text">$</span>
+            </b-input-group-prepend>
+            <b-form-input
+              id="investment_amount"
+              v-model="investment_amount"
+              :state="hasInvest"
+              type="number"
+              name="investment_amount"
+            />
+          </b-input-group>
+        </b-form-group>
+
+        <b-button :disabled="!hasInvest" type="submit" variant="primary">Invest</b-button>
+      </b-form>
+    </section>
 
     <section class="investment_funds">
       <h3 class="subtitle">Loan Funds</h3>
-      <ul>
-        <!-- TODO: list all funds -->
-      </ul>
+      <b-table v-if="funds.length > 0" :fields="fields" :items="funds">placeholder</b-table>
+      <b v-else span>No funds found</b>
     </section>
-    
-    <p>
-      <b-button type="button" vairant="success" @click="goBack">
-        Back
-      </b-button>
-    </p>
 
+    <p>
+      <b-button type="button" vairant="success" @click="goBack">Back</b-button>
+    </p>
   </b-container>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      investment_amount: 0,
+      fields: ['id', 'amount', 'created_on']
+    }
+  },
+  computed: {
+    hasInvest() {
+      return this.investment_amount > 99
+    }
+  },
   async asyncData({ $axios, params }) {
     let investment = await $axios.get(`/api/investment/${params.id}`)
     let funds = await $axios.get(`/api/investment/${params.id}/funds`)
@@ -31,6 +59,24 @@ export default {
     }
   },
   methods: {
+    async onSubmit(ev) {
+      ev.preventDefault()
+      if (!this.hasInvest) {
+        return
+      }
+      let { id, investment_amount } = this
+      let data = {
+        investment_id: id,
+        amount: investment_amount
+      }
+      console.log(data)
+      let invest = await this.$axios({
+        method: 'post',
+        url: '/api/funding',
+        data
+      })
+      this.$router.go()
+    },
     goBack() {
       this.$router.back()
     }
